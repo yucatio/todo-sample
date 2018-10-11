12914de47ddb053710c6234188f97fd16c5a9a3e
Yuka Saito <yucatio.com@gmail.com>
2018-10-11 23:35:13 +0900

    Changed Link to NavLink

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { NavLink, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 let Navbar = ({uid}) => (
   <div>
-    <Link to='/'>Home</Link>&nbsp;
-    { uid && <Link to={`/users/${uid}/todos`}>タスクを編集する</Link> }
+    <NavLink exact to='/' activeStyle={{fontWeight: "bold", color: "#DF3A01"}}>Home</NavLink>&nbsp;
+    { uid && <NavLink exact to={`/users/${uid}/todos`} activeStyle={{fontWeight: "bold", color: "#DF3A01"}}>タスクを編集する</NavLink> }
   </div>
 )
 
@@ -18,8 +18,8 @@ const mapStateToProps = state => (
   { uid: state.firebase.auth.uid }
 )
 
-Navbar = connect(
+Navbar = withRouter(connect(
   mapStateToProps
-)(Navbar)
+)(Navbar))
 
 export default Navbar;
