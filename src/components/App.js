7e02c2b33c2bfb1e46524932205a6c493ecdc7f8
Yuka Saito <yucatio.com@gmail.com>
2018-10-11 09:28:58 +0900

    Remove react-router from dependency

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch } from 'react-router'
+import { Route, Switch } from 'react-router-dom'
 import { ConnectedRouter } from 'connected-react-router'
 import Navbar from './Navbar'
 import Dashboard from './Dashboard'
